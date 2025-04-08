21f6f812f029a2e009aed0b78e7aa8fa89d91d87
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-04-08 20:02:06 +0200

    ADD:decorator cash

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_list = {}
+
+    @functools.wraps(func)
+    def inner(*args, **kwargs) -> None:
+        key = (args, frozenset(kwargs.items()))
+        if func.__name__ not in cache_list:
+            cache_list[func.__name__] = {}
+
+        if key in cache_list[func.__name__]:
+            print("Getting from cache")
+            return cache_list[func.__name__][key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_list[func.__name__][key] = result
+            return result
+    return inner
